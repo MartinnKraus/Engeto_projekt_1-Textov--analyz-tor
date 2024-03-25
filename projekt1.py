a8d6a4b287e53d5d058b5c35b8fabfd294150342
MartinnKraus <martinnkraus@gmail.com>
2024-03-25 22:26:51 +0100

    Jeden cyklus při procházení jednotlivých slov -> statistiky ukládané a následně dotahované z knihovny "statistiky"

--- projekt1.py
+++ projekt1.py
@@ -7,7 +7,7 @@ discord: martin_64789
 
 import sys
 
-import task_template    #Import knihovny s textem ze zadání
+from task_template import TEXTS   #Import knihovny s textem ze zadání
 from users import passwords #Import knihovny s hesly
 
 def has_numbers(inputString):
@@ -24,13 +24,12 @@ def count_by_len(seznam, delka):
 
 
 username = input("Insert username: ") #zjisti username
-
+password = input("Insert password: ") #zjisti heslo
 if username not in passwords.keys():    #ověř, zda je uživatel registrovaný
     sys.exit("Unregistered user, terminating the program..")
 
-password = input("Insert password: ") #zjisti heslo
 
-if passwords[username] != password:
+elif passwords[username] != password:
     #Ukončí program, pokud je uživatel neregistrovaný
     sys.exit("Wrong password, terminating the program..")
 else:
@@ -52,33 +51,36 @@ else:
         cislo_textu = int(cislo_textu)
     print("-" * 40 )
 #Přiřaď text do proměnné a pročisti:
-    text = task_template.TEXTS[cislo_textu - 1]
+    text = TEXTS[cislo_textu - 1]
     slova = [slovo.strip(",.:;-") for slovo in text.split()]
     slova_set = set(slova)
-#Výpočty:
-    pocet_slov = len(slova) # Celkový počet slov
-
-    pocet_titlecase = len([slovo for slovo in slova if slovo.istitle()])    #Počet slov počínajících velkým písmenem
-
-    pocet_uppercase = len([slovo for slovo in slova if slovo.isupper() and not has_numbers(slovo)])    #Počet slov psaných velkým písmem
-
-    pocet_lowercase = len([slovo for slovo in slova if slovo.islower()])    #Počet slov psaných malým písmem
-
-    pocet_numeric = len([slovo for slovo in slova if slovo.isnumeric()])
-
-    soucet_cisel = sum(int(cislo) for cislo in slova if cislo.isnumeric())
+#Statistiky:
+    #new - ulož do knihovny:
+    statistiky = {}
+    statistiky["pocet_slov"] = len(slova)
+    for slovo in slova:
+        if slovo.istitle():
+            statistiky["pocet_titlecase"] = statistiky.setdefault("pocet_titlecase", 0) + 1
+        if slovo.isupper() and not has_numbers(slovo):
+            statistiky["pocet_uppercase"] = statistiky.setdefault("pocet_uppercase", 0) + 1
+        if slovo.islower():
+            statistiky["pocet_lowercase"] = statistiky.setdefault("pocet_lowercase", 0) + 1
+        if slovo.isnumeric():
+            statistiky["pocet_numeric"] = statistiky.setdefault("pocet_numeric", 0) + 1
+        if slovo.isnumeric():
+            statistiky["soucet_cisel"] = statistiky.setdefault("soucet_cisel", 0) + int(slovo)
 
 #Knihovna s délkami slov:
     max_delka = max(len(slovo) for slovo in slova)
-    pocet_delek = {delka : count_by_len(slova, delka) for delka in range(1, max_delka + 1) if count_by_len(slova, delka) > 0}
+    pocet_delek = {delka: count_by_len(slova, delka) for delka in range(1, max_delka + 1) if count_by_len(slova, delka) > 0}
     max_vyskytu = max(pocet_delek.values())
 #Printy výsledků:
-    print(f"There are {pocet_slov} words in the selected text.",
-        f"There are {pocet_titlecase} titlecase words.",
-        f"There are {pocet_uppercase} uppercase words.",
-        f"There are {pocet_lowercase} lowercase words.",
-        f"There are {pocet_numeric} numeric strings.",
-        f"The sum of all the numbers {soucet_cisel}",
+    print(f"There are {statistiky.get("pocet_slov")} words in the selected text.",
+        f"There are {statistiky.get("pocet_titlecase")} titlecase words.",
+        f"There are {statistiky.get("pocet_uppercase")} uppercase words.",
+        f"There are {statistiky.get("pocet_lowercase")} lowercase words.",
+        f"There are {statistiky.get("pocet_numeric")} numeric strings.",
+        f"The sum of all the numbers {statistiky.get("soucet_cisel")}",
         sep="\n"
         )
 #Vytiskni "graf" délek:
@@ -88,10 +90,6 @@ else:
           sep="\n"
           )
     for delka in pocet_delek:
-        print(str(delka).rjust(3),
-              "|",
-              ("*" * pocet_delek[delka]).ljust(max_vyskytu + 3),
-              "|",
-              pocet_delek[delka],
-              sep = ""
+        print(str(delka).rjust(3), "|", ("*" * pocet_delek[delka]).ljust(max_vyskytu + 3),
+              "|", pocet_delek[delka], sep = ""
               )
